848207aa82708f9b519a6f332bb7376ebdfe9e53
Andy <89816884+lya0108@users.noreply.github.com>
2023-02-17 12:29:02 +1300

    Add files via upload

---
+++ 00_MM_base.py
@@ -0,0 +1,20 @@
+# functions
+def choice_checker(question, valid_list, error):
+
+    valid = False
+    while not valid:
+        
+        # asks user for choice
+        response = input(question).lower()
+
+        # checks if input is in list
+        for item in valid_list:
+            if response == item[0] or response == item:
+                return item
+        
+        print(error)
+        print()
+
+maybe = ["yes", "no"]
+
+intro = choice_checker("Do You Want To ")
---
+++ 02_loop.py
@@ -0,0 +1,6 @@
+game = "yes"
+while game == "yes":
+    name = input("Please Enter Your Name or 'xxx' to quit: ")
+
+    if name == "xxx":
+        break
